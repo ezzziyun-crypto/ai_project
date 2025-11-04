c356e143266ebfbcbfca769b67dcd4f1b6c120f6
ezzziyun-crypto <ezzziyun@gmail.com>
2025-11-04 15:04:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
-st.write('안녕하세요! 만나서 반갑습니다!')
+name=st.text_input ('이름을 입력하세요:')
+menu=st.selectbox('좋아하는 음식을 선택해주세요:',['마라탕','짜빠게티'])
+if st.button('인사말 생성'):
+  st.info(name+'님! 안녕하세요')
+  st.warning(menu+를 좋아하시나봐요! 저도 좋아해요!')
+  st.error('정말 반가워요')
+  st.ballonns()
